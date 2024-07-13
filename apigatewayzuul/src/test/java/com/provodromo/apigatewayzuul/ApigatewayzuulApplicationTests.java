f06e409ac1cb2207641935a273512873af25236c
herlmanoel <herlmanoel@MacBook-Air-de-Herlmanoel.local>
2024-07-13 13:15:30 -0300

    init provodromo

---
+++ apigatewayzuul/src/test/java/com/provodromo/apigatewayzuul/ApigatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provodromo.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
