071b4db15b4a9010bb24a658259ad630810ceb7f
herlmanoel <herlmanoel@MacBook-Air-de-Herlmanoel.local>
2024-08-06 15:53:28 -0300

    feat: add db_provodromo

--- init_db.sql
+++ init_db.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE provodromo_ms_db;
+CREATE USER admin WITH PASSWORD 'provodromo_pass';
+
+CREATE DATABASE db_provodromo OWNER admin;
+
+GRANT ALL PRIVILEGES ON DATABASE db_provodromo TO admin;
